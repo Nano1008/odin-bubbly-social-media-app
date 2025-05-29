43a58c08744082344c807be8fd1b6d4e1f99c79b
Sinan <nanossn1008@gmail.com>
2025-05-29 22:05:21 +0100

    Update number of comments immediately

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -83,7 +83,7 @@ function Post({
         </button>
 
         <button onClick={handleExistingComment}>
-          {comments.length} comments
+          {commentsList.length} comments
         </button>
       </div>
 
