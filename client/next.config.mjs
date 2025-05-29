abc86610524cd796dbd82ebf9fa8cd720c54b053
Sinan <nanossn1008@gmail.com>
2025-05-29 17:18:05 +0100

    Add image domain

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.jsdelivr.net", "picsum.photos"],
+    domains: ["avatars.githubusercontent.com", "cdn.jsdelivr.net"],
   },
 };
 
