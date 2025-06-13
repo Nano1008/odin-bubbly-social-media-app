efef750aca37845475a09c1e730c10a998a1d1cc
Sinan <nanossn1008@gmail.com>
2025-06-14 00:00:53 +0100

    Add google image search

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com", "cdn.jsdelivr.net"],
+    domains: ["avatars.githubusercontent.com", "cdn.jsdelivr.net", "lh3.googleusercontent.com"],
   },
 };
 
